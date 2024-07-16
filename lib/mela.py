cbc5b46fbdf97eb60a71c8b28213ac64c3514776
root <a.p.goloborodko@gmail.com>
2024-07-16 19:15:48 +0500

    Firmware for 16MB board added. RTC (DS1302) class added with NOW function implementation in human readable form. Information class added with DF anf FREE implementation in human readable form (VFS and RAM info)

--- lib/mela.py
+++ lib/mela.py
@@ -1,16 +1,93 @@
 from bmp180 import BMP180
-from machine import SoftI2C, Pin
-import dht
+from machine import SoftI2C, Pin 
+import dht 
+import ds1302
+import utime
+import gc
+import os
+#======================================================================================================
 
+
+
+
+#--------------------------------------------------------------------
+#
+#   Main class for our board
+#
+#--------------------------------------------------------------------
 class Mela:
   def __init__(self):
-    self.environment=Environment()
+    self.environment=Mela_environment()
+    self.rtc=Mela_rtc()
+    self.info=Mela_info()
 
-class Environment:
+#--------------------------------------------------------------------
+#
+#    Information class for our board
+#
+#--------------------------------------------------------------------
+class Mela_info:
+  def __init__(self):
+    if not gc.isenabled(): gc.enable()
+    gc.collect()
+
+  @property
+  def df(self):
+    s = os.statvfs('//')
+    T=(s[1]*s[2])/1048576
+    F=(s[0]*s[3])/1048576
+    P = '{0:.2f}%'.format(F/T*100)
+    return ('VFS: Total:{0} Mb Free:{1} Mb ({2})'.format(T,F,P))
+
+  @property
+  def free(self):
+    gc.collect()
+    F = gc.mem_free()
+    A = gc.mem_alloc()
+    T = F+A
+    P = '{0:.2f}%'.format(F/T*100)
+    return ('RAM: Total:{0} bytes Free:{1} bytes ({2})'.format(T,F,P))
+
+
+#--------------------------------------------------------------------
+#
+#    RTC class for DS1302
+#
+#--------------------------------------------------------------------
+class Mela_rtc:
+  def __init__(self):
+    self.__ds1302_rtc = ds1302.DS1302(Pin(21), Pin(22), Pin(23))  # (clk, dio, cs)
+
+# convert library output to human readable form
+  @property
+  def now(self):
+    return str(self.__ds1302_rtc.year())+"-"+str('%0*d' % (2, self.__ds1302_rtc.month()))+"-"+str('%0*d' % (2, self.__ds1302_rtc.day()))+" "+str('%0*d' % (2, self.__ds1302_rtc.hour()))+":"+str('%0*d' % (2, self.__ds1302_rtc.minute()))+":"+str('%0*d' % (2, self.__ds1302_rtc.second())) 
+
+# convert library output to standart micropython tuple (year, month, mday, hour, minute, second, weekday, yearday)
+  @property
+  def now_raw(self):
+    return [self.__ds1302_rtc.year(),self.__ds1302_rtc.month(),self.__ds1302_rtc.day(),self.__ds1302_rtc.hour(),self.__ds1302_rtc.minute(),self.__ds1302_rtc.second(),self.__ds1302_rtc.weekday(),0] 
+
+# convert library output to standart unix unixtime
+  @property
+  def now_unixtime(self):
+    return utime.mktime(self.now_raw)+946684800
+
+# convert library output to unixtime for Embeded boards (since 2000-01-01)
+  @property
+  def now_em_unixtime(self):
+    return utime.mktime(self.now_raw)
+
+#--------------------------------------------------------------------
+#
+#    Environment class for MBM180
+#
+#--------------------------------------------------------------------
+class Mela_environment:
   def __init__(self):
     i2c = SoftI2C(scl=Pin(18), sda=Pin(19), freq=100000)
     self.__bmp180 = BMP180(i2c)
-    self._t_h = dht.DHT22(Pin(20))
+    self.__dht = dht.DHT11(Pin(14))
 
   @property
   def temperature(self):
@@ -26,9 +103,5 @@ class Environment:
 
   @property
   def humidity(self):
-    self._t_h.measure()
-    return self._t_h.humidity()
-
-
-plc = Mela()
-plc.pressure()
+    self.__dht.measure()
+    return self.__dht.humidity()
