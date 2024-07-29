c1c7efdfbb8d44a0b2a11adf1b83a6a5ba8f608a
root <a.p.goloborodko@gmail.com>
2024-07-29 19:52:23 +0500

    first version of settings saving class added

--- lib/mela/mela.py
+++ lib/mela/mela.py
@@ -1,5 +1,6 @@
 import gc
 import os
+import ujson as json
 #======================================================================================================
 
 
@@ -13,6 +14,49 @@ import os
 class Mela:
   def __init__(self):
     self.info=Mela_info()
+    self.settings=Mela_settings()
+    self.wifi=False
+ 
+    try:
+      if self.settings.wifi['connect_on_boot']:
+        import network
+        sta_if=network.WLAN(network.STA_IF)
+        sta_if.active(True)
+        #wlan_networks=sta_if.scan()
+        #print(wlan_networks)
+        sta_if.connect(self.settings.wifi['ssid'],self.settings.wifi['key'])
+        self.wifi=sta_if
+    except:
+      print('WLAN connection problem')
+
+#--------------------------------------------------------------------
+#
+#    Settings save/load class for our board
+#
+#--------------------------------------------------------------------
+class Mela_settings:
+  def __init__(self):
+    self.data=self.load_settings()
+
+  @property
+  def wifi(self):
+    return self.data['settings']['wifi']
+
+  def load_settings(self):
+    try:
+      with open('settings.json','r') as settings_file:
+        return json.load(settings_file)
+    except:
+      print("Settings file read problem. Returning default configuration.")
+      return {'settings': {'wifi': {'connect_on_boot': False, 'ssid': 'LAN', 'key': '12345'}}} 
+
+  def save_settings(self):
+    try:
+      with open('settings.json','w') as settings_file:
+        return json.dump(self.data,settings_file)
+    except:
+      print("Settings file write problem.")
+      return False
 
 #--------------------------------------------------------------------
 #
@@ -24,7 +68,6 @@ class Mela_info:
     if not gc.isenabled(): gc.enable()
     gc.collect()
 
-  @property
   def df(self):
     s = os.statvfs('//')
     T=(s[1]*s[2])/1048576
@@ -32,7 +75,6 @@ class Mela_info:
     P = '{0:.2f}%'.format(F/T*100)
     return ('VFS: Total:{0} Mb Free:{1} Mb ({2})'.format(T,F,P))
 
-  @property
   def free(self):
     gc.collect()
     F = gc.mem_free()
