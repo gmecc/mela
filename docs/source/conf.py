4f0ff92fdb00a678eff225d98c211a5dd4751d73
Sergey Besedin <67929399+gmecc@users.noreply.github.com>
2024-11-06 09:43:00 +0100

    file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,7 +26,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# import ushahidi_sphinx_rtd_theme
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
+# html_theme = 'ushahidi_sphinx_rtd_theme'
+html_static_path = ['_static']
